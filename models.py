ce3058d27d430c242c25b5db9d24947b879ab1fe
DemAntoine <demydenko.anton@gmail.com>
2019-12-14 18:38:20 +0200

    1

--- models.py
+++ models.py
@@ -1,12 +1,33 @@
 from peewee import SqliteDatabase, IntegerField, Model, DateTimeField, CharField, ForeignKeyField, BooleanField, fn
 from datetime import datetime
 from playhouse.sqlite_ext import SqliteExtDatabase
+from peewee import chunked
 
-db = SqliteDatabase('users.db', pragmas={'foreign_keys': 1})
-
-
+# db_old = SqliteDatabase('users.db', pragmas={'foreign_keys': 1})
+#
+#
 # def time_format():
 #     return datetime.now().strftime('%y.%m.%d %H:%M:%S.%f')[:-4]
+#
+#
+# class Petition_old(Model):
+#     class Meta:
+#         database = db_old
+#         db_table = "petition"
+#
+#     petition_id = IntegerField()
+#     position_number = IntegerField()
+#     username = CharField()
+#     sign_date = CharField()
+#
+#     def __str__(self):
+#         return f'{self.petition_id}: {self.position_number} {self.username} {self.sign_date}'
+
+
+db = SqliteExtDatabase('petitions.db', pragmas=(
+    ('cache_size', -1024 * 64),  # 64MB page-cache.
+    ('journal_mode', 'wal'),  # Use WAL-mode (you should always use this!).
+    ('foreign_keys', 1)), regexp_function=True)  # Enforce foreign-key constraints.
 
 
 class Petition(Model):
@@ -18,50 +39,25 @@ class Petition(Model):
     position_number = IntegerField()
     username = CharField()
     sign_date = CharField()
-    
 
     def __str__(self):
         return f'{self.petition_id}: {self.position_number} {self.username} {self.sign_date}'
-
-
-
-db_2 = SqliteExtDatabase('my_app.db', pragmas=(
-    ('cache_size', -1024 * 64),  # 64MB page-cache.
-    ('journal_mode', 'wal'),  # Use WAL-mode (you should always use this!).
-    ('foreign_keys', 1)),  regexp_function=True)  # Enforce foreign-key constraints. 
-
-
-class Petition2(Model):
-    class Meta:
-        database = db_2
-        db_table = "petition"
-
-    petition_id = IntegerField()
-    position_number = IntegerField()
-    username = CharField()
-    sign_date = CharField()
-    
-
-    def __str__(self):
-        return f'{self.petition_id}: {self.position_number} {self.username} {self.sign_date}'
-
-
-
-
 
 
 if __name__ == '__main__':
+    # db_old.create_tables([Petition_old], safe=True)
     db.create_tables([Petition], safe=True)
-    db_2.create_tables([Petition2], safe=True)
-    
 
-    query = Petition.select()
-    
-    for i in range(200, 100000, 15):
-        Petition2.create(
-                petition_id = query.where(Petition.id==i)[0].petition_id,
-                position_number = query.where(Petition.id==i)[0].position_number,
-                username = query.where(Petition.id==i)[0].username,
-                sign_date = query.where(Petition.id==i)[0].sign_date
-            )
-    
+    # query = Petition_old.select()
+    #
+    # # for n in range(0, 2200000, 100000):
+    # data = []
+    # for i in query[2200000 + 1:]:
+    #     data.append(
+    #         (i.petition_id, i.position_number, i.username, i.sign_date)
+    #     )
+    # print('append')
+    # with db.atomic():
+    #     # by default SQLite limits the number of bound variables in a SQL query to 999
+    #     for batch in chunked(data, 990):
+    #         Petition.insert_many(batch).execute()
--- tmp.py
+++ tmp.py
@@ -5,15 +5,17 @@ import re
 import requests
 import csv
 import json
-from models import Petition, Petition2
+from models import Petition
 import time
 from peewee import fn
 from collections import Counter
 
-
 # total rows
-# total_rows = Petition.select().count()
+# total_rows = Petition_old.select().count()
 # print('Total rows', total_rows)
+total_rows = Petition.select().count()
+print('Total rows', total_rows)
+
 
 # petitions list
 def petitions_list():
@@ -21,28 +23,32 @@ def petitions_list():
     petitions = [i.petition_id for i in petitions]
     return petitions
 
-# print('Len: ', len(petitions_list()))
+
+print('Len: ', len(petitions_list()))
+
 
 # name contain substring
 def name_contain(substring):
-    query = Petition2.select().where(Petition2.username.regexp(substring))
-    
-    
+    query = Petition.select().where(Petition.username.regexp(substring))
+
     print(f'Found: {len(query)} with substring {substring}')
     show = int(input('Show (1), escape(0): '))
     if show:
         for i in query:
             print(f'{i.petition_id}: {i.username}')
 
+
 name_contain(r'Дем.+янчук.+С')
 
-# # most frequent names
-# query = (Petition
-#          .select(Petition.username, fn.COUNT(Petition.petition_id).alias('count'))
-#          .group_by(Petition.username)
-#          .order_by(fn.COUNT(Petition.petition_id).desc()))
-# for row in query[:10]:
-#     print(row.username, row.count)
+
+# most frequent names
+query = (Petition
+         .select(Petition.username, fn.COUNT(Petition.petition_id).alias('count'))
+         .group_by(Petition.username)
+         .order_by(fn.COUNT(Petition.petition_id).desc()))
+for row in query[:10]:
+    print(row.username, row.count)
+
 
 # count by petitions
 def petitions_count():
@@ -51,13 +57,12 @@ def petitions_count():
     print(f'There is {len(petitions_list)} petitions in db')
     p_dict = dict()
     for i in petitions_list:
-        p_dict[i] = query.where(Petition.petition_id==i).count()
-        
+        p_dict[i] = query.where(Petition.petition_id == i).count()
+
     p_dict = {k: v for k, v in sorted(p_dict.items(), key=lambda item: item[1])}
-    
+
     for i in p_dict.items():
         print(i)
-    
-# petitions_count()
 
-  
+
+petitions_count()
